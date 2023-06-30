735404f2c1ac592929f6e3729d8b415b6c4b51fe
skatta3 <56640542+skatta3@users.noreply.github.com>
2023-06-30 07:34:16 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-aws s3 cp s3://codebuild-bucket123/JavaProj/target/java-tomcat-maven-example.war /tmp
+aws s3 cp s3://mybucket2-29jun/HelloWorld/java-tomcat-maven-example.war /tmp
 sudo mv /tmp/java-tomcat-maven-example.war /var/lib/tomcat9/webapps/java-tomcat-maven-example.war
 sudo service tomcat9 restart
