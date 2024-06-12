42143805319b17c81f4d3ffe3665c51500d6ed60
skazerwa <skazerwa@gmail.com>
2024-06-12 12:19:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-aws s3 cp s3://mycodebuildbucketuseast1/HelloWorld/java-tomcat-maven-example.war /tmp
+aws s3 cp s3://bnkbucket01/bnkartifact1/java-tomcat-maven-example.war /tmp
 sudo mv /tmp/java-tomcat-maven-example.war /var/lib/tomcat10/webapps/java-tomcat-maven-example.war
 sudo service tomcat10 restart
